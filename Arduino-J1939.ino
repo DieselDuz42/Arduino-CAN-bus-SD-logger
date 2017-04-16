bc0d7f258e70b0c18dfe37ee697ffe420c4c1cab
John Oliver <DieselDuz42@users.noreply.github.com>
2017-04-16 02:14:52 -0400

    Removed identifier length detection. Closes #2 J1939 only uses extended identifiers.

--- Arduino-J1939.ino
+++ Arduino-J1939.ino
@@ -119,19 +119,12 @@ void loop()
   {
     CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
     
-    if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
       sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
-    else
-      sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
-  
-    Serial.print(msgString);
-    dataFile.print(msgString);
-  
-    if((rxId & 0x40000000) == 0x40000000){    // Determine if message is a remote request frame.
-      sprintf(msgString, " REMOTE REQUEST FRAME");
-      Serial.print(msgString);
+	  // print the message
+	  Serial.print(msgString);
       dataFile.print(msgString);
     } else {
+	  // loop through the data buffer
       for(byte i = 0; i<len; i++){
         sprintf(msgString, " 0x%.2X", rxBuf[i]);
         Serial.print(msgString);
