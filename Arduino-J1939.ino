8f9028a56308d376a43f26b26b76e6915e52a79e
John Oliver <DieselDuz42@users.noreply.github.com>
2017-04-19 21:38:11 -0400

    log files were filling up with blank lines, added check to see if the data collected is blank to avoid empty lines. Also added the use of the F() function to save space when enabling the serial console

--- Arduino-J1939.ino
+++ Arduino-J1939.ino
@@ -12,7 +12,7 @@
 #define CAN0_INT 2 
 #define SD_CHIP_SELECT 9
 MCP_CAN CAN0(10);
-#define SERIAL_ON 0
+#define SERIAL_ON 1
 
 
 long unsigned int rxId;
@@ -34,13 +34,13 @@ boolean initSD(void)
   if (!SD.begin(SD_CHIP_SELECT))
   {
     #if SERIAL_ON
-    Serial.println("Card failed, or not present");
+      Serial.println(F("Card failed, or not present"));
 	  #endif
   }
   else
   {
     #if SERIAL_ON
-	  Serial.println("Card initialized.");
+	    Serial.println(F("Card initialized."));
 	  #endif
   }
 }
@@ -60,20 +60,20 @@ void setup()
   Serial.begin(115200);
   initSD();
   #if SERIAL_ON
-  Serial.println("Beginning to log");
-  Serial.print("Checking file name: ");
-  Serial.println(fileName);
+    Serial.println(F("Beginning to log"));
+    Serial.print(F("Checking file name: "));
+    Serial.println(fileName);
   #endif
   boolean fileExists = SD.exists(fileName);
   if ( fileExists == true )
   {
     for (uint8_t i = 0; i < 100; i++)
     {
-      fileName[5] = i / 10 + '0';
-      fileName[6] = i % 10 + '0';
+      fileName[4] = i / 10 + '0';
+      fileName[5] = i % 10 + '0';
       #if SERIAL_ON
-	    Serial.print("Checking file name: ");
-      Serial.println(fileName);
+	      Serial.print(F("Checking file name: "));
+        Serial.println(fileName);
 	    #endif
       fileExists = SD.exists(fileName);
       if (fileExists == false )
@@ -94,16 +94,16 @@ void setup()
   // Initialize MCP2515 running at 16MHz with a baudrate of 250kb/s and the masks and filters disabled.
   if (CAN0.begin(MCP_ANY, CAN_250KBPS, MCP_16MHZ) == CAN_OK)
     #if SERIAL_ON
-    Serial.println("MCP2515 Initialized Successfully!");
+      Serial.println(F("MCP2515 Initialized Successfully!"));
     else
-    Serial.println("Error Initializing MCP2515...");
+      Serial.println(F("Error Initializing MCP2515..."));
 	#endif
 	
   CAN0.setMode(MCP_LISTENONLY);                     
   pinMode(CAN0_INT, INPUT);                            
   #if SERIAL_ON
-  Serial.println("MCP2515 Library Receive Example...");
-  boolean setupSuccess = true;
+    Serial.println(F("MCP2515 Ready"));
+    boolean setupSuccess = true;
   #endif
 }
 
@@ -113,34 +113,36 @@ void loop()
   if (!digitalRead(CAN0_INT))                        
   {
     CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+    if (&rxId>0)
+    {
       lastmsgrcv = millis();          // set the last recived var
       tdiff = gettdiff();            // set the time difference
-      sprintf(tbuff, "%ld,%ld,", millis(), tdiff); // add the times to the message
-      #if SERIAL_ON
-  	  Serial.print(tbuff);
-	    #endif
-      dataFile.print(tbuff);
-      sprintf(msgString, "0x%.8lX,%1d,", (rxId & 0x1FFFFFFF), len); // formats the message
+      //sprintf(tbuff, "%ld,%ld,", millis(), tdiff); // add the times to the message
+      //#if SERIAL_ON
+  	  //Serial.print(tbuff);
+	    //#endif
+      //dataFile.print(tbuff);
+      sprintf(msgString, "%ld,%ld,0x%.8lX,%1d,", millis(), tdiff, (rxId & 0x1FFFFFFF), len); // formats the message
 	    #if SERIAL_ON
-	    Serial.print(msgString);        // print the compiled message to serial
+	      Serial.print(msgString);        // print the compiled message to serial
 	    #endif
       dataFile.print(msgString);      // print the compiled message to file
-      
-
       for (byte i = 0; i < len; i++)
         {    // loop through the data buffer
           sprintf(msgString, "0x%.2X,", rxBuf[i]);// format the data buffer
           #if SERIAL_ON
-	        Serial.print(msgString);        // print the formatted data to serial
+	          Serial.print(msgString);        // print the formatted data to serial
 	        #endif
           dataFile.print(msgString);        // print the formatted data to file
         }
     
-  }
+  
   #if SERIAL_ON
-  Serial.println();         // prints newline to serial
+    Serial.println();         // prints newline to serial
   #endif
   dataFile.println();           // prints newline to file
+    }
+  }
 }
 
 
